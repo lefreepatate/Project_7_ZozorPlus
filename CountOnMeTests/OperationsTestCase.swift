11ab7f4ea7a774fa95e07b8853f6c3e3b49b4409
cgmuskat <c_garcia_muskat@hotmail.com>
2018-12-18 12:52:44 +0100

    Adding new test for all operators (x,÷,+,-) with 100% coverage

--- CountOnMeTests/OperationsTestCase.swift
+++ CountOnMeTests/OperationsTestCase.swift
@@ -24,9 +24,6 @@ class OperationsTestCase: XCTestCase {
       operations.stringNumbers.append("")
       text = operations.updateDisplay()
    }
-   func setCalculateTotal() {
-      text = "\(operations.calculateTotal())"
-   }
    // MARK: - TEST DDB
    func testGivenTextIsNull_WhenIncrementingNumber5_ThenTextIs5() {
       text = ""
@@ -42,7 +39,7 @@ class OperationsTestCase: XCTestCase {
       // Then
       XCTAssertEqual(text, "5×")
    }
-   func testGivenTextIs5_WhenIncrementingX5_ThenTextIs5X5() {
+   func testGivenTextIs5_WhenIncrementingMultiply5_ThenTextIs5X5() {
       setNewNumber(5)
       //When
       setOperatorValue("×")
@@ -50,7 +47,24 @@ class OperationsTestCase: XCTestCase {
       // Then
       XCTAssertEqual(text, "5×5")
    }
-   func testGivenTextIs5X5_WhenIncrementingTotal_ThenTextIs25() {
+   func testGivenTextIs50_WhenIncrementingPlus45_ThenTotalIs95() {
+      setNewNumber(5)
+      setNewNumber(0)
+      //When
+      setOperatorValue("+")
+      setNewNumber(45)
+      // Then
+      XCTAssertEqual(operations.calculateTotal(), 95)
+   }
+   func testGivenTextIs4_WhenIncrementingMinus5_ThenTotalIsNegative1() {
+      setNewNumber(4)
+      //When
+      setOperatorValue("-")
+      setNewNumber(5)
+      // Then
+      XCTAssertEqual(operations.calculateTotal(), -1)
+   }
+   func testGivenTextIs5_WhenIncrementingMultiply5_ThenTotalIs25() {
       setNewNumber(5)
       //When
       setOperatorValue("×")
@@ -58,4 +72,23 @@ class OperationsTestCase: XCTestCase {
       // Then
       XCTAssertEqual(operations.calculateTotal(), 25)
    }
+   func testGivenTextIs30_WhenIncrementingDivide5_ThenTotalIs6() {
+      setNewNumber(3)
+      setNewNumber(0)
+      //When
+      setOperatorValue("÷")
+      setNewNumber(5)
+      // Then
+      XCTAssertEqual(operations.calculateTotal(), 6)
+   }
+   func testGivenTextIs50_WhenIncrementingDivide63_ThenTotalIsFloat() {
+      setNewNumber(5)
+      setNewNumber(0)
+      //When
+      setOperatorValue("÷")
+      setNewNumber(63)
+      // Then
+      XCTAssertEqual(operations.calculateTotal(), 0.7936508)
+   }
+
 }
